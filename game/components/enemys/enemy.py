1ef187f2a4fc6fb1a76259b46e763c99172a771c
dev1001world <dev.world1001@hotmail.com>
2023-07-15 00:49:46 -0500

    Second-Class
    add see in class

---
+++ game/components/enemys/enemy.py
@@ -0,0 +1,41 @@
+from typing import Any
+import pygame, random
+from pygame.sprite import Sprite
+from game.utils.constants import ENEMY_1, SHIP_WIDTH, SHIP_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT
+class Enemy(Sprite):
+    
+    POS_Y= 10 
+    SPEED_X = 2.5
+    SPEED_Y = 0.5
+    MOVE_X = {0: 'left', 1:'right'}
+    def __init__(self):
+        self.image = ENEMY_1
+        self.image = pygame.transform.scale(self.image,(SHIP_WIDTH,SHIP_HEIGHT))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(1,SCREEN_WIDTH)
+        self.rect.y = self.POS_Y
+        self.speed_x = self.SPEED_X
+        self.speed_y = self.SPEED_Y
+        self.movement_x = self.MOVE_X[random.randint(0,1)]
+        self.move_for_x = random.randint(50, 100)
+        self.step = 0
+    def update(self, enemies):
+        self.rect.y += self.speed_y
+        if self.rect.y >= SCREEN_HEIGHT:
+            enemies.remove(self)
+        
+        if self.movement_x == 'left':
+            self.rect.x -= self.speed_x
+        else:
+            self.rect.x += self.speed_x
+        self.change_movement_x()
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+    def change_movement_x(self):
+        self.step += 1
+        if (self.step >= self.move_for_x and self.movement_x == 'right') or (self.rect.x >= SCREEN_WIDTH-SHIP_WIDTH):
+            self.movement_x = 'left'
+            self.step = 0
+        if (self.step >= self.move_for_x and self.movement_x == 'left') or (self.rect.x <= 0):
+            self.movement_x = 'right'
+            self.step = 0
--- game/components/space_ship.py
+++ game/components/space_ship.py
@@ -1,28 +1,38 @@
 import pygame
 from pygame.sprite import Sprite
-from game.utils.constants import SPACESHIP, SCREEN_HEIGHT, SCREEN_WIDTH
+from game.utils.constants import SPACESHIP, SCREEN_HEIGHT, SCREEN_WIDTH, SHIP_WIDTH, SHIP_HEIGHT
 class Spaceshipt(Sprite):
+    X_POS = (SCREEN_WIDTH // 2) - 40
+    Y_POS = SCREEN_HEIGHT - 100
+    SPEED_SHIP = 10
     def __init__(self):
         self.imgage = SPACESHIP
-        self.imgage = pygame.transform.scale(self.imgage,(40,60))
+        self.imgage = pygame.transform.scale(self.imgage,(SHIP_WIDTH,SHIP_HEIGHT))
         self.rect = self.imgage.get_rect()
-        self.rect.x = (SCREEN_WIDTH // 2) - 40
-        self.rect.y = SCREEN_HEIGHT - 100
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS
     def update(self,user_input):
-        if user_input[pygame.K_LEFT]:
-            self.rect.x -=10
-            if self.rect.x < -45:
-                self.rect.x = SCREEN_WIDTH
-        elif user_input[pygame.K_RIGHT]:
-            self.rect.x +=10
-            if self.rect.x > SCREEN_WIDTH:
-                self.rect.x = 0
-
-        if user_input[pygame.K_UP]:
-            if self.rect.y > SCREEN_HEIGHT // 3: #deje screenheing // 3  para poder crear una mecanica de esquivar naves enemigas
-                self.rect.y -= 10
-        elif user_input[pygame.K_DOWN]:
-            if self.rect.y < SCREEN_HEIGHT - 70:
-                self.rect.y += 10
+        if user_input[pygame.K_LEFT] or user_input[pygame.K_a]:
+            self.move_left()
+        if user_input[pygame.K_RIGHT] or user_input[pygame.K_d]:
+            self.move_right()
+        if user_input[pygame.K_UP] or user_input[pygame.K_w]:
+            self.move_up()
+        if user_input[pygame.K_DOWN] or user_input[pygame.K_s]:
+             self.move_down()
+    def move_left(self):
+        self.rect.x -=self.SPEED_SHIP
+        if self.rect.x < -45:
+            self.rect.x = SCREEN_WIDTH
+    def move_right(self):
+        self.rect.x +=self.SPEED_SHIP
+        if self.rect.x > SCREEN_WIDTH:
+            self.rect.x = SCREEN_WIDTH - SCREEN_WIDTH
+    def move_up(self):
+        if self.rect.y > SCREEN_HEIGHT // 3: #deje screenheing // 3  para poder crear una mecanica de esquivar naves enemigas
+                self.rect.y -= self.SPEED_SHIP
+    def move_down(self):
+        if self.rect.y < SCREEN_HEIGHT - 70:
+                self.rect.y += self.SPEED_SHIP
     def draw(self, screen):
         screen.blit(self.imgage,self.rect)
